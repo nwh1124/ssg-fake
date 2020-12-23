b131a0eeea224bc19a8eb4b7b6c65f97a00a7cf0
nwh1124 <nwh0716@gmail.com>
2020-12-23 22:04:04 +0900

    content

--- app.js
+++ app.js
@@ -36,4 +36,25 @@ function viewer_index() {
 	});
 }
 
+function viewer_index_2() {
+	
+	var viewerEl = document.querySelector('.main .content:nth-child(2)');
+	
+	if ( viewerEl == null ) {
+		return;
+	}
+	
+	var body = viewerEl.innerHTML;
+	
+	var viewer = new toastui.Editor.factory({
+		el : viewerEl,
+		initialValue : body,
+		viewer : true,
+	    plugins: [toastui.Editor.plugin.codeSyntaxHighlight]
+	});
+}
+
 viewer_index();
+
+viewer_index_2.normalize();
+viewer_index_2();
